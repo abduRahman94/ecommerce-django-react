a3ac38dd7cb3bd3006f1245974211e2caaa2a71a
Abdou SEYE <abdou.seye@gsietechnology.com>
2023-03-19 19:03:59 +0000

    Mise en place projet et modeles

---
+++ shop/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Categorie(models.Model):
+    libelle = models.CharField(max_length=200)
+
+
+class Produit(models.Model):
+    libelle = models.CharField(max_length=200)
+    prix = models.FloatField(null=True)
+    description = models.TextField(null=True)
+    quantite_stock = models.IntegerField(null=True)
+    categorie = models.ForeignKey(Categorie, on_delete=models.PROTECT)
+
+
+class Client(User):
+    
+    def passer_commande(self):
+        pass
+    
+    def annuler_commande(self):
+        pass
+    
+    def modifier_commande(self):
+        pass
+
+class Commande(models.Model):
+    class StatusCommande(models.IntegerChoices):
+        VALIDE = 1
+        ANNULE = 2
+        ATTENTE = 3
+    
+    code_commande = models.CharField(max_length=200)
+    client = models.ForeignKey(Client)
+    produit = models.ForeignKey(Produit)
+    statut_commande = models.IntegerField(choices=StatusCommande.choices)
+    
+
+
+    
